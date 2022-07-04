eb9a8bd3ce7785187b7b58e7ef92a0fddd4bc8fc
Édher Paulinelli Fernandes <48846865+edherpaulinelli@users.noreply.github.com>
2022-07-04 11:37:28 -0300

    Adição da Tela Carrinho Cliente

---
+++ app/src/main/java/udesc/diskgaseagua/view/client/ViewCarrinho.java
@@ -0,0 +1,17 @@
+package udesc.diskgaseagua.view.client;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import udesc.diskgaseagua.R;
+
+
+public class ViewCarrinho extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_carrinho);
+    }
+}
+
+
--- app/src/main/java/udesc/diskgaseagua/view/client/ViewPedidosCliente.java
+++ app/src/main/java/udesc/diskgaseagua/view/client/ViewPedidosCliente.java
@@ -2,15 +2,49 @@ package udesc.diskgaseagua.view.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import udesc.diskgaseagua.R;
 
 public class ViewPedidosCliente extends AppCompatActivity {
 
+    private Button btSomenteAgua, btSomenteGas, btAmbos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos_cliente);
+
+
+        btSomenteAgua = findViewById(R.id.btSomenteAgua);
+        btSomenteGas = findViewById(R.id.btSomenteGas);
+        btAmbos = findViewById(R.id.btAmbos);
+
+        btSomenteAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewPedidosCliente.this, ViewCarrinho.class);
+                startActivity(intent);
+            }
+        });
+
+        btSomenteGas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewPedidosCliente.this, ViewCarrinho.class);
+                startActivity(intent);
+            }
+        });
+
+        btAmbos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewPedidosCliente.this, ViewCarrinho.class);
+                startActivity(intent);
+            }
+        });
     }
 }
